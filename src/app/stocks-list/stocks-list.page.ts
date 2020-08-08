370dbb6d92043723f60e8916b94e50ba294ffb08
jorgeunimicro <jorge@unimicro.no>
2020-08-08 10:26:33 +0200

    simple stocks list

--- src/app/stocks-list/stocks-list.page.ts
+++ src/app/stocks-list/stocks-list.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StocksService } from '../stocks.service';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil, distinctUntilChanged, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-stocks-list',
@@ -7,12 +10,49 @@ import { StocksService } from '../stocks.service';
   styleUrls: ['./stocks-list.page.scss'],
 })
 export class StocksListPage implements OnInit {
-  public data = null;
-  
+  public data = [];
+  public filteredData = [];
+  public searchFormControl = new FormControl('');
+  private onDestroy$ = new Subject();
   constructor(public stocksService: StocksService) { }
 
   ngOnInit() {
-    this.stocksService.getData().subscribe(_data => this.data = _data);
+    this.stocksService.getData().subscribe(_data => {
+      this.data = _data
+      const keys = Object.keys(this.data);
+      this.filteredData = keys.map(key => this.data[key]);
+    });
+    this.searchFormControl.valueChanges.pipe(
+      startWith(''),
+      takeUntil(this.onDestroy$),
+      distinctUntilChanged()
+    ).subscribe(query => {
+      if (query === '') {
+        this.filteredData = this.data; 
+      }
+      const keys = Object.keys(this.data);
+      this.filteredData = keys.filter(this.selectIfQueryMatches(query)).map(key => this.data[key]);
+    });
   }
 
+  selectIfQueryMatches(query) {
+    const _query = query.toLowerCase();
+    return (key) => {
+      if(!this.filteredData.length) {
+        return true;
+      }
+        return this.data[key].title.toLowerCase().startsWith(_query)
+        || this.data[key].name.toLowerCase().startsWith(_query)
+        || this.data[key].market.toLowerCase().startsWith(_query);
+    };
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  trackByStock(index: number, stock: any) {
+    return stock.name;
+  }
 }
